62149480f42973221f1bab6a9a8065a7fcdd1326
DAVICOREI <ribeirodavivargas@gmail.com>
2026-06-03 12:02:30 -0300

    fix: corrige nome da variavel no post

--- src/main/java/com/daviribeiro/ecommerce3d/controllers/ProdutoController.java
+++ src/main/java/com/daviribeiro/ecommerce3d/controllers/ProdutoController.java
@@ -3,8 +3,11 @@ package com.daviribeiro.ecommerce3d.controllers;
 import com.daviribeiro.ecommerce3d.entities.Produto;
 import com.daviribeiro.ecommerce3d.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.daviribeiro.ecommerce3d.services.FinanceiroService;
@@ -45,5 +48,11 @@ public class ProdutoController {
             
             return map;
         }).collect(Collectors.toList());
+    }
+    @PostMapping
+    public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
+        // Usando a variável 'repository' que foi declarada na linha 25
+        Produto novoProduto = repository.save(produto);
+        return ResponseEntity.status(201).body(novoProduto);
     }
 }
