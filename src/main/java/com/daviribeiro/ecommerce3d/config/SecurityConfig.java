de1d9addcaf7879ea96d81651e86250771774cf0
DAVICOREI <ribeirodavivargas@gmail.com>
2026-06-03 22:30:50 -0300

    fix: corrige bloqueio de CORS no Spring Security

--- src/main/java/com/daviribeiro/ecommerce3d/config/SecurityConfig.java
+++ src/main/java/com/daviribeiro/ecommerce3d/config/SecurityConfig.java
@@ -1,8 +1,9 @@
-package com.daviribeiro.ecommerce3d.config; // Ajuste o pacote de acordo com o seu projeto
+package com.daviribeiro.ecommerce3d.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,20 +16,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> csrf.disable()) // Desabilita CSRF pois usaremos Tokens (JWT)
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // API REST não guarda estado/sessão
+            .cors(Customizer.withDefaults()) // 1. Avisa o Security para usar a nossa classe CorsConfig
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-                // 1. Quem quiser ver os produtos (GET) ou fazer checkout (POST), está liberado
+                // 2. Libera a requisição invisível (OPTIONS) para o navegador não surtar
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
+                
                 .requestMatchers(HttpMethod.GET, "/api/produtos").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/checkout/pagar").permitAll()
-                
-                // 2. A rota onde faremos login também precisa ser pública
                 .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                 
-                // 3. Cadastrar novos produtos (POST /api/produtos) EXIGE autenticação!
                 .requestMatchers(HttpMethod.POST, "/api/produtos").authenticated()
-                
-                // Qualquer outra requisição que não listamos acima também precisará de login
                 .anyRequest().authenticated()
             )
             .build();
