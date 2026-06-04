63b36e075223a8160c5e115b7add92e4a196dcb2
DAVICOREI <ribeirodavivargas@gmail.com>
2026-06-04 20:21:26 -0300

    fix: corrige nome da variavel do banco de dados no controller

--- src/main/java/com/daviribeiro/ecommerce3d/controllers/ProdutoController.java
+++ src/main/java/com/daviribeiro/ecommerce3d/controllers/ProdutoController.java
@@ -4,8 +4,10 @@ import com.daviribeiro.ecommerce3d.entities.Produto;
 import com.daviribeiro.ecommerce3d.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +56,15 @@ public class ProdutoController {
         // Usando a variável 'repository' que foi declarada na linha 25
         Produto novoProduto = repository.save(produto);
         return ResponseEntity.status(201).body(novoProduto);
+    }
+    // ... seu código anterior (métodos GET e POST)
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
